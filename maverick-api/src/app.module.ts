1a863cf47b079c742b897cf7bba5be7cfb80cf68
João Lucas <jlcarruda3@gmail.com>
2022-08-17 18:32:14 -0300

    feat: Docker compose functional for API

--- maverick-api/src/app.module.ts
+++ maverick-api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [TerminusModule, ConfigModule.forRoot()],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
